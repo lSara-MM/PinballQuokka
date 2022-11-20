7822b2d463547a9ebd1bc3cd22494d11d2055204
SoniaOL <99950351+SoniaOL@users.noreply.github.com>
2022-11-20 20:00:59 +0100

    retrymusic fix

--- MartinezSara_NosasAndreu_SegoviaEric_OjedaSonia_HidalgoAlberto/ModuleScene.cpp
+++ MartinezSara_NosasAndreu_SegoviaEric_OjedaSonia_HidalgoAlberto/ModuleScene.cpp
@@ -377,6 +377,9 @@ bool ModuleScene::Start()
 	audiomiau = App->audio->LoadFx("pinball/meow.ogg");
 	audiolose = App->audio->LoadFx("pinball/gameOver.ogg");
 
+	if (retry == true) {
+		App->audio->PlayMusic("pinball/Music.ogg", 0);
+	}
 	greenP = false;
 	purpleP = false; 
 	turquoiseP = false; 
